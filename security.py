19adeda61dcdabfca5d49744ea32c5ecef9cfd21
Jack Wagner <jackwagner@Jacks-Air.attlocal.net>
2024-11-26 15:23:26 -0600

    #10 cleanup security class definition

--- security.py
+++ security.py
@@ -1,5 +1,3 @@
-from logging import logger
-
 class Security:
     '''
     The Security object represents a tradable financial asset
@@ -9,24 +7,12 @@ class Security:
 
     Attributes:
         ticker(str): The ticker the strategy determines when to buy/sell on
-        name (str): Name of the asset
     '''
-    def __init__(self, ticker) -> None:
+    def __init__(self, ticker: str) -> None:
         self.ticker = ticker
-
-        self.name = None
-
 
     def __str__(self):
-        return f"Strategy {self.name} on {self.ticker}"
-
-    def set_ticker(self, ticker):
-        '''Change the ticker without recreating a strategy object'''
-        # :TO-DO create Asset class, which can generate historical/real-time performance data from Ticker
-        self.ticker = ticker
-
-    def get_name(self):
-        return self.name
+        return f"Security with ticker {self.ticker}"
     
     def get_ticker(self):
         return self.ticker
