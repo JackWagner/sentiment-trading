983443a984e20067eef6642312f173e5a0f1cd99
Jack Wagner <jackwagner@Jacks-Air.attlocal.net>
2024-11-27 00:19:37 -0600

    #5 create discord channel class

---
+++ scrapers/discord/channel.py
@@ -0,0 +1,28 @@
+# get web scraper
+from scrapers.web_scraper import WebScraper
+
+# import channel IDs and authorizaton
+from os import path
+from json import load
+
+# temporary local user config
+homedir          = path.expanduser('~')
+discord_user_conf = open(path.join(homedir,'.config/sst/discord_channels.json'),'r')
+discord_user      = load(discord_user_conf, strict=False)
+
+class Channel(WebScraper):
+    def __init__(self, channel_id):
+        self.url = f'https://discord.com/api/v9/channels/{channel_id}'
+        self.headers = {
+            'authorization': discord_user.get("authorization")
+        }
+        super().__init__(self.url, self.headers)
+
+    def get_messages(self):
+        '''
+        Returns df (message_time, message_contents) of messages over [start,end]
+        '''
+        html = self.get_html()
+        soup = self.parse_html(html)
+        data = self.extract_data(soup)
+        return self.extract_data(soup)
